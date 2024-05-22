f317435872c402292271f371d373164ea4281b08
suhiaing <suhlaingthin288@gmail.com>
2024-05-22 11:26:09 +0630

    remove extra layoutBuilder

--- lib/pages/second_page/second_page.dart
+++ lib/pages/second_page/second_page.dart
@@ -23,116 +23,113 @@ class Second extends StatelessWidget {
             color: const Color.fromARGB(255, 234, 232, 232),
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 2.0),
-              child: LayoutBuilder(builder: (context, constraints) {
-                return Scrollbar(
-                  thumbVisibility: true,
-                  trackVisibility: true,
-                  thickness: 5,
-                  controller: scrollController,
-                  child: ListView.builder(
-                      controller: scrollController,
-                      itemCount: plan["habbits"][0]["data"].length,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, itemCount) {
-                        return Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Container(
-                              height: 150,
-                              width: 300,
-                              decoration: const BoxDecoration(
-                                color: Colors.white,
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.grey,
-                                    offset: Offset(4, 4),
-                                    blurRadius: 15,
-                                    spreadRadius: 5,
-                                  ),
-                                  BoxShadow(
-                                    color: Colors.white,
-                                    offset: Offset(-4, -4),
-                                    blurRadius: 15,
-                                    spreadRadius: 5,
-                                  )
-                                ],
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(150),
-                                  topRight: Radius.circular(150),
+              child: Scrollbar(
+                thumbVisibility: true,
+                trackVisibility: true,
+                thickness: 5,
+                controller: scrollController,
+                child: ListView.builder(
+                    controller: scrollController,
+                    itemCount: plan["habbits"][0]["data"].length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, itemCount) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Container(
+                            height: 150,
+                            width: 300,
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey,
+                                  offset: Offset(4, 4),
+                                  blurRadius: 15,
+                                  spreadRadius: 5,
                                 ),
+                                BoxShadow(
+                                  color: Colors.white,
+                                  offset: Offset(-4, -4),
+                                  blurRadius: 15,
+                                  spreadRadius: 5,
+                                )
+                              ],
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(150),
+                                topRight: Radius.circular(150),
                               ),
-                              child: Center(
-                                child: Text(
-                                  "Day\n  ${(itemCount + 1).toString()}",
-                                  style: const TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 30,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Day\n  ${(itemCount + 1).toString()}",
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
                             ),
-                            Container(
-                              width: 380,
-                              height: 550,
-                              decoration: BoxDecoration(
-                                border: Border(
-                                  left: BorderSide(
-                                      width: 1.0, color: Colors.grey.shade500),
-                                  right: BorderSide(
-                                      width: 1.0, color: Colors.grey.shade50),
-                                ),
-                                color: Colors.white,
+                          ),
+                          Container(
+                            width: 380,
+                            height: 550,
+                            decoration: BoxDecoration(
+                              border: Border(
+                                left: BorderSide(
+                                    width: 1.0, color: Colors.grey.shade500),
+                                right: BorderSide(
+                                    width: 1.0, color: Colors.grey.shade50),
                               ),
-                              child: Column(
-                                children: [
-                                  Container(
-                                    width: 100,
-                                    height: 50,
-                                    decoration: BoxDecoration(
-                                        color: Colors.lightGreen.shade400,
-                                        borderRadius:
-                                            BorderRadius.circular(10)),
-                                    child: Center(
-                                      child: Text(
-                                        plan["habbits"][0]["data"][itemCount]
-                                            ["date"],
-                                        style: const TextStyle(
-                                            fontSize: 17, color: Colors.white),
-                                      ),
+                              color: Colors.white,
+                            ),
+                            child: Column(
+                              children: [
+                                Container(
+                                  width: 100,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                      color: Colors.lightGreen.shade400,
+                                      borderRadius: BorderRadius.circular(10)),
+                                  child: Center(
+                                    child: Text(
+                                      plan["habbits"][0]["data"][itemCount]
+                                          ["date"],
+                                      style: const TextStyle(
+                                          fontSize: 17, color: Colors.white),
                                     ),
                                   ),
-                                  Expanded(
-                                      child: ListView.separated(
-                                    itemCount: plan["habbits"].length,
-                                    separatorBuilder:
-                                        (BuildContext context, int index) =>
-                                            const SizedBox(
-                                      height: 10,
-                                    ),
-                                    itemBuilder:
-                                        (BuildContext context, int index) {
-                                      return Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            Text(plan["habbits"][index]
-                                                ["habbitName"]),
-                                          ],
-                                        ),
-                                      );
-                                    },
-                                  )),
-                                ],
-                              ),
-                            )
-                          ],
-                        );
-                      }),
-                );
-              }),
+                                ),
+                                Expanded(
+                                    child: ListView.separated(
+                                  itemCount: plan["habbits"].length,
+                                  separatorBuilder:
+                                      (BuildContext context, int index) =>
+                                          const SizedBox(
+                                    height: 10,
+                                  ),
+                                  itemBuilder:
+                                      (BuildContext context, int index) {
+                                    return Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(plan["habbits"][index]
+                                              ["habbitName"]),
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                )),
+                              ],
+                            ),
+                          )
+                        ],
+                      );
+                    }),
+              ),
             ),
           );
         }),
